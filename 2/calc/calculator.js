492349f7943f94671478a962fc47d67e13f5823a
bcole <coleb29@gator.uhd.edu>
2020-01-14 21:41:02 +0000

    eslint

--- 2/calc/calculator.js
+++ 2/calc/calculator.js
@@ -3,31 +3,42 @@ const readline = require('readline-sync');
 console.log('Welcome to Calculator!');
 console.log(performOperation(getInput()));
 
+function checkValidity(firstNumber, secondNumber, operation) {
+  if (Number.isFinite(Number(firstNumber)) &&
+  Number.isFinite(Number(secondNumber)) && Number.isFinite(Number(operation))) {
+   return true;
+  } else {
+    console.clear();
+    console.log("The provided information is invalid. Please enter it again.");
+    return false;
+  }
+}
+
 function getInput() {
-    let haveNumbers=false, firstNumber, secondNumber, operation;
-    while(!haveNumbers) {
-        console.log('What\'s the first number?');
-        firstNumber = readline.question();
+  let haveNumbers = false;
+  let firstNumber;
+  let secondNumber;
+  let operation;
+  while ( !haveNumbers ) {
+  console.log('What\'s the first number?');
+  firstNumber = readline.question();
         console.log(`The first number is ${firstNumber}. What is the second number?`);
         secondNumber = readline.question();
-        console.log(`The first number is ${firstNumber}. The second number is ${secondNumber}. What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`);
+        console.log(`The first number is ${firstNumber}. The second number is ${secondNumber}.`);
+        console.log(` What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`);
         operation = readline.question();
-        if(Number.isFinite(Number(firstNumber)) && Number.isFinite(Number(secondNumber)) && Number.isFinite(Number(operation)))
-            haveNumbers=true;
-        else {
-            	console.clear();
-		console.log("The provided information is invalid. Please enter it again.")
-	}
+        haveNumbers = checkValidity(firstNumber,secondNumber,operation);
     }
+
     return [Number(firstNumber),Number(secondNumber),Number(operation)];
 }
 
 function performOperation(values) {
-    switch(values[2]) {
+    switch (values[2]) {
         case 1:
             return `The result is : ${values[0] + values[1]}`;
         case 2:
-            return `The result is : ${values[0] - values[1]}`; 
+            return `The result is : ${values[0] - values[1]}`;
         case 3:
             return `The result is : ${values[0] * values[1]}`;
         case 4:
@@ -37,6 +48,3 @@ function performOperation(values) {
 
     }
 }
-
-
-
