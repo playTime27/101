922d555cd3c239e7300d4843561339c0d3f81659
Cole <coleb29@academic.uhd.campus>
2020-01-14 20:17:17 -0600

    send2server

--- 2/mortgageCalc/mortgage.js
+++ 2/mortgageCalc/mortgage.js
@@ -1,1 +1,21 @@
 const readline = require('readline-sync');
+
+function startCalculator() {
+    prompt('test');
+}
+
+function getLoanAmount() {
+
+}
+
+function getAPR() {
+
+}
+
+function getLoanDuration() {
+
+}
+
+function prompt(message) {
+    console.log('\x1b[32m%s\x1b[31m%s', '$=>', message);
+}
