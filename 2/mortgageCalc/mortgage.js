b5fcd4599b495935e810c6c68ee693bde177ddda
Cole <coleb29@academic.uhd.campus>
2020-01-14 20:58:15 -0600

    send2server input test

--- 2/mortgageCalc/mortgage.js
+++ 2/mortgageCalc/mortgage.js
@@ -1,9 +1,12 @@
 const readline = require('readline-sync');
+const messages = require('./messages.json');
 
 startCalculator();
 
 function startCalculator() {
-    prompt('test');
+  askForInput('loanAmount');
+  askForInput('apr');
+  askForInput('loanDuration');
 }
 
 function getLoanAmount() {
@@ -18,6 +21,60 @@ function getLoanDuration() {
 
 }
 
+function askForInput(value) {
+  switch (value) {
+    case "loanAmount":
+        setLoanAmount(readInput(messages.loan.amount));
+      break;
+    case "apr":
+        setAPR(readInput(messages.loan.apr));
+      break;
+    case "loanDuration":
+        setLoanDuration(readInput(messages.loan.duration));
+      break;
+    default:
+      //generalInput?
+  }
+}
+
+function readInput(message) {
+  prompt(message);
+  let response = readline.question();
+  return response;
+}
+
+function invalidInput() {
+    console.clear();
+    prompt("The input you entered is not correct. Please try again.");
+}
+
+function setLoanAmount(response) {
+  if (response.trim() !== "" && Number(response)) {
+    return response;
+  } else {
+      invalidInput();
+      askForInput('loanAmount');
+  }
+}
+
+function setAPR(response) {
+    if (response.trim() !== "" && Number(response)) {
+        return response;
+      } else {
+          invalidInput();
+          askForInput('apr');
+      }
+}
+
+function setLoanDuration(response) {
+    if (response.trim() !== "" && Number(response)) {
+        return response;
+      } else {
+          invalidInput();
+          askForInput('loanDuration');
+      }
+}
+
 function prompt(message) {
-    console.log('\x1b[32m%s\x1b[31m%s\x1b[0m', '$=>', message);
+    console.log(messages.colorCode, messages.prompt, message);
 }
